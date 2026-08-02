5891738bad3083e3156b033d33cd6e1f6f4f4d52
MeghanaChowdary21 <saimeghanachowdary21@gmail.com>
2026-08-02 17:44:24 +0530

    Added simple calculator program

---
+++ 03_Conditional_Statements/calculator.py
@@ -0,0 +1,16 @@
+# Simple Calculator
+
+a = 20
+b = 10
+choice = "+"
+
+if choice == "+":
+    print(a + b)
+elif choice == "-":
+    print(a - b)
+elif choice == "*":
+    print(a * b)
+elif choice == "/":
+    print(a / b)
+else:
+    print("Invalid Choice")
