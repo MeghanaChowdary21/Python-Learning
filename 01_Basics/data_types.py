4996a1783354d688b4aa86f17d860550fa3adc24
MeghanaChowdary21 <saimeghanachowdary21@gmail.com>
2026-08-02 17:36:23 +0530

    Added data types example

---
+++ 01_Basics/data_types.py
@@ -0,0 +1,11 @@
+# Data Types Example
+
+integer_num = 10
+float_num = 15.5
+string_value = "Python"
+boolean_value = True
+
+print(type(integer_num))
+print(type(float_num))
+print(type(string_value))
+print(type(boolean_value))
