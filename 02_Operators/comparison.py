6eb2cff70cc2dedb82ee59709fb1056cde8247ed
MeghanaChowdary21 <saimeghanachowdary21@gmail.com>
2026-08-02 17:38:43 +0530

    Added comparison operators example

---
+++ 02_Operators/comparison.py
@@ -0,0 +1,11 @@
+# Comparison Operators
+
+x = 15
+y = 10
+
+print("x == y :", x == y)
+print("x != y :", x != y)
+print("x > y :", x > y)
+print("x < y :", x < y)
+print("x >= y :", x >= y)
+print("x <= y :", x <= y)
