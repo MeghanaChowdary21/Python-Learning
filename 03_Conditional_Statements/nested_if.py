a735c21b0f2a299b01ca1564e6f108881b8a2f9d
MeghanaChowdary21 <saimeghanachowdary21@gmail.com>
2026-08-02 17:42:36 +0530

    Added nested if example

---
+++ 03_Conditional_Statements/nested_if.py
@@ -0,0 +1,12 @@
+# Nested If Example
+
+age = 22
+has_id = True
+
+if age >= 18:
+    if has_id:
+        print("Entry Allowed")
+    else:
+        print("ID Required")
+else:
+    print("Under Age")
